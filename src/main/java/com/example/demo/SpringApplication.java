08d64c5e40c2ae7b0aecc4e065b175329a9adeae
isabella_tavares <isabella_tavares@estudante.sesisenai.org.br>
2024-06-03 20:19:32 -0300

    03/06/2024 - aula Banco

--- src/main/java/com/example/demo/SpringApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-public class SpringApplication {
-
-    public static void run(Class<DemoApplication> class1, String[] args) {
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
-    }
-
-}
