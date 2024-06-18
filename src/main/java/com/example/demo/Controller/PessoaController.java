00d22c51a278cd1e4b88d171b782a0906ca5ddba
isabella_tavares <isabella_tavares@estudante.sesisenai.org.br>
2024-06-17 21:40:46 -0300

    17/06/24

--- src/main/java/com/example/demo/Controller/PessoaController.java
+++ src/main/java/com/example/demo/Controller/PessoaController.java
@@ -1,14 +1,19 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.Model.Pessoa;
+import com.example.demo.Repository.PessoaRepository;
 
 
 @Controller
 public class PessoaController {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     @GetMapping("/pessoa/create")
     public String create() {
@@ -18,7 +23,6 @@ public class PessoaController {
     @PostMapping("/pessoa/create")
     public String create(Pessoa pessoa) {
         System.out.println(pessoa.getNome());
-
         return "pessoa/create";
     }
 }
